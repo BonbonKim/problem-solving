a6f782b35c3e1a8ef75a5cfdc8940ea89a0d09b4
bonbon <boeun0613@gmail.com>
2022-02-23 03:26:32 +0900

    Inflearn : 다시 푼 문제
    02_07

--- Inflearn/02_코드구현력/p_02_07.py
+++ Inflearn/02_코드구현력/p_02_07.py
@@ -1,24 +1,23 @@
 '''
 7. 소수(에라토스테네스 체)
 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램
--> 시간 초과, 다시 풀어보기
 ''' 
 
 import time
 
 start_time = time.time()
 num = int(input())
-prime_num_list = list()
 
-for i in range(2, num + 1):
-    is_prime_num = True
-    for j in range(2, i):
-        if i % j == 0:
-            is_prime_num = False
-            break
-    if is_prime_num:
-        prime_num_list.append(i)
+prime_num_list = [0] * (num+1)
+prime_cnt = 0
 
-print(len(prime_num_list))
+for i in range(2, num+1):
+    if prime_num_list[i] == 0:
+        prime_cnt += 1
+        for j in range(i, num+1, i):
+            prime_num_list[j] += 1
+
+print(prime_cnt)
+
 end_time = time.time()
 # print("time", end_time-start_time)
