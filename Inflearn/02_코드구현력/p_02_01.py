be825f2ecc87857810fe3790ec9cf45b174904de
bonbon <boeun0613@gmail.com>
2022-03-14 20:49:44 +0900

    인프런 다시 풀기
    p_02_01

--- Inflearn/02_코드구현력/p_02_01.py
+++ Inflearn/02_코드구현력/p_02_01.py
@@ -3,18 +3,8 @@
 두 개의 자연수 N과 K가 주어졌을 때, N의 약수들 중 K번째로 작은 수를 출력하는 프로그램
 '''
 
-N, K = input().split()
-N, K = int(N), int(K)
-num_list = list()
+n, k = map(int, input().split())
+num = [i for i in range(1, n+1) if n%i == 0]
 
-for i in range(1, N+1):
-    if N % i == 0:
-        num_list.append(i)
-num_list.sort()
-
-if len(num_list) < K-1:
-    print(-1)
-else:
-    print(num_list[K-1])
-
-
+answer = num[k-1] if len(num)>=k else -1
+print(answer)
