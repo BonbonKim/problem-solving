861c68ed2f5a366f221dc9522397a28191d0d02d
bonbon <boeun0613@gmail.com>
2022-04-02 22:21:01 +0900

    인프런 다시 풀기

--- Inflearn/02_코드구현력/p_02_05.py
+++ Inflearn/02_코드구현력/p_02_05.py
@@ -4,24 +4,13 @@
 가장 확률이 높은 숫자를 출력하는 프로그램
 '''
 
-num_list = list(map(int, (input().split())))
-N, M = num_list[0], num_list[1]
-combi_dict = {}
+from collections import Counter
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        sum_num = i+j
-        if sum_num in combi_dict:
-            combi_dict[sum_num] = combi_dict[sum_num] + 1
-        else:
-            combi_dict[sum_num] = 1
+n, m = map(int, input().split())
+nums = []
+for i in range(1, n+1):
+    nums += [i+j for j in range(1, m+1)]
 
-combi_dict = sorted(combi_dict.items(), key = lambda item:item[1], reverse=True)
-
-results = list()
-for i in range(len(combi_dict)):
-    if combi_dict[0][1] != combi_dict[i][1]:
-        break
-    results.append(combi_dict[i][0])
-
-print(" ".join(map(str, results)))
+count = Counter(nums).most_common()
+answer = [str(c[0]) for c in count if c[1]==count[0][1]]
+print(" ".join(answer))
--- Inflearn/02_코드구현력/p_02_06.py
+++ Inflearn/02_코드구현력/p_02_06.py
@@ -4,25 +4,12 @@ N개의 자연수가 입력되면 각 자연수의 자릿수의 합을 구하고
 그 합이 최대인 자연수를 출력하는 프로그램
 '''
 
-def sum_digit(x):
-    x = str(x)
-    ans = 0
-    for i in range(len(x)):
-        ans += int(x[i])
-    return ans
+nums_len = int(input())
+nums = list(input().split())
 
+for i in range(nums_len):
+    sum_n = sum(list(map(int, nums[i])))
+    nums[i] = [nums[i], sum_n]
 
-max_num = 0
-save_num = -1
-len_num = int(input())
-num_list = list(map(int,input().split()))
-
-for i in range(len_num-1, -1, -1):
-    cur_num = sum_digit(num_list[i])
-    if max_num <= cur_num:
-        max_num = cur_num
-        save_num = i
-
-print(num_list[save_num])
-
-
+nums.sort(reverse=True, key=lambda x: x[1])
+print(nums[0][0])
