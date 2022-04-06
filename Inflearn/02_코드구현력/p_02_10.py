9fe2bf407bc428ac4b6c67c878988fc19aa95cb3
bonbon <boeun0613@gmail.com>
2022-04-06 20:47:43 +0900

    인프런 다시 풀기

--- Inflearn/02_코드구현력/p_02_10.py
+++ Inflearn/02_코드구현력/p_02_10.py
@@ -3,18 +3,13 @@
 시험문제의 채점 결과가 주어졌을 때, 총 점수를 계산하는 프로그램
 '''
 
-N = int(input())
-score_list = list(map(int, input().split()))
+_ = input()
+num = list(map(int,input().split()))
 
-result_sum = 0
-accumu_cnt = 0
-for i in range(N):
-    cur_score = score_list[i]
-
-    if cur_score == 1:
-        accumu_cnt = accumu_cnt + 1
-        result_sum += accumu_cnt
+answer = [0]
+for n in num:
+    if n == 0:
+        answer.append(0)
     else:
-        accumu_cnt = 0
-
-print(result_sum)
+        answer.append(answer[-1]+1)
+print(sum(answer))
