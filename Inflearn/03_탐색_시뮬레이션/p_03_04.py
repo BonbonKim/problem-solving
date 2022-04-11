92b2029561090ce05e76d7877c889617fe7e46ab
bonbon <boeun0613@gmail.com>
2022-04-11 23:48:47 +0900

    인프런 다시 풀기

--- Inflearn/03_탐색_시뮬레이션/p_03_04.py
+++ Inflearn/03_탐색_시뮬레이션/p_03_04.py
@@ -3,11 +3,10 @@
 오름차순으로 정렬이 된 두 리스트가 주어지면 두 리스트를 오름차순으로 합쳐 출력하는 프로그램
 '''
 
-n = input()
-list_n = list(map(int, input().split()))
-m = input()
-list_m = list(map(int, input().split()))
+_ = input()
+nums = input() + " "
+_ = input()
+nums += input()
+nums = list(map(int, nums.split()))
+print(" ".join(map(str,sorted(nums))))
 
-list_all = list_n + list_m
-list_all = sorted(list_all)
-print(" ".join(map(str, list_all)))
